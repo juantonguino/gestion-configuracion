4b0e032f06055e617e1d13fd7311e1d0bb8938be
Juan Diego Tonguino <juan.tonguino@gmail.com>
2023-08-14 21:08:11 -0500

    Se adiciona clase persona

---
+++ demo/src/main/java/com/example/demo/Persona.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import java.util.Date;
+
+public class Persona {
+
+    private int edad;
+
+    private String nombre;
+
+    private Date fechaNacimiento;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+}
